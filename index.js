15b6d95721a2f5227b8cd5362bea715e96a9745a
Jacek Dziewanowski <dziewanowski@gmail.com>
2018-04-14 15:37:14 +0200

    change greeting

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hello: 'world!' });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
