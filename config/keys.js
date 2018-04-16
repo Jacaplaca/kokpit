496f767adea00dfc34c91abf3fa632871da1352d
Jacek Dziewanowski <dziewanowski@gmail.com>
2018-04-16 14:26:33 +0200

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// key.js - which keys i shoutl commit
+
+if (process.env.NODE_ENV === 'production') {
+  // we are in production
+  module.exports = require('./prod');
+} else {
+  // we are in dev
+  module.exports = require('./dev');
+}
