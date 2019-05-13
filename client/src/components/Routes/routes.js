23e270c9e3dc2175162802639d558046c1e4c804
Jacaplaca <dziewanowski@gmail.com>
2019-05-13 22:30:05 +0200

    only start

--- client/src/components/Routes/routes.js
+++ client/src/components/Routes/routes.js
@@ -26,14 +26,14 @@ const routes = [
     component: Start,
     id: 0
     // title: "Start"
-  },
-  {
-    comp: "costs",
-    path: "/costs",
-    component: Costs,
-    id: 8
-    // title: "Dodaj koszty"
-  },
+  }
+  // {
+  //   comp: "costs",
+  //   path: "/costs",
+  //   component: Costs,
+  //   id: 8
+  //   // title: "Dodaj koszty"
+  // },
   // {
   //   comp: "planer_reports",
   //   path: "/planer",
@@ -48,118 +48,118 @@ const routes = [
   //   id: 3
   //   // title: "Dodaj raport z aktywności"
   // },
-  {
-    comp: "nextReports",
-    path: "/nextreports",
-    component: NextReports,
-    id: 4
-    // title: ""
-  },
-  {
-    comp: "powerBi",
-    path: "/powerbi",
-    component: PowerBI,
-    id: 22
-    // title: ""
-  },
   // {
-  //   comp: "serwis",
-  //   path: "/serwis",
-  //   component: Serwis,
-  //   title: "Dodaj transakcję dla serwisu",
-  //   channel: auth ? auth.channel_first : 0
+  //   comp: "nextReports",
+  //   path: "/nextreports",
+  //   component: NextReports,
+  //   id: 4
+  //   // title: ""
+  // },
+  // {
+  //   comp: "powerBi",
+  //   path: "/powerbi",
+  //   component: PowerBI,
+  //   id: 22
+  //   // title: ""
+  // },
+  // // {
+  // //   comp: "serwis",
+  // //   path: "/serwis",
+  // //   component: Serwis,
+  // //   title: "Dodaj transakcję dla serwisu",
+  // //   channel: auth ? auth.channel_first : 0
+  // // },
+  // // {
+  // //   comp: "bonus_system",
+  // //   path: "/channelconfiguration",
+  // //   component: ChanProdConf,
+  // //   title: "Skonfiguruj kanały sprzedaży",
+  // //   id: 20
+  // //   //channel: auth ? auth.channel_first : 0
+  // // },
+  // {
+  //   comp: "bonus_system",
+  //   path: "/products",
+  //   component: Products,
+  //   id: 25
+  //   // title: "Produkty"
+  //   //channel: auth ? auth.channel_first : 0
   // },
   // {
   //   comp: "bonus_system",
-  //   path: "/channelconfiguration",
-  //   component: ChanProdConf,
-  //   title: "Skonfiguruj kanały sprzedaży",
-  //   id: 20
+  //   path: "/systems",
+  //   component: Channels,
+  //   id: 26
+  //   // title: "Systemy prowizyjne"
   //   //channel: auth ? auth.channel_first : 0
   // },
-  {
-    comp: "bonus_system",
-    path: "/products",
-    component: Products,
-    id: 25
-    // title: "Produkty"
-    //channel: auth ? auth.channel_first : 0
-  },
-  {
-    comp: "bonus_system",
-    path: "/systems",
-    component: Channels,
-    id: 26
-    // title: "Systemy prowizyjne"
-    //channel: auth ? auth.channel_first : 0
-  },
-  {
-    comp: "bonus_system",
-    path: "/users_channels",
-    component: UsersChannels,
-    id: 27
-    // id: 20
-    // title: "Dodaj koszty"
-  },
-  {
-    comp: "users",
-    path: "/users",
-    component: Users,
-    id: 1
-    // title: "Użytkownicy"
-    //channel: auth ? auth.channel_first : 0
-  },
   // {
-  //   comp: "channels_config",
-  //   path: "/configs",
-  //   component: ChannelsConfig,
-  //   title: "Konfiguracja systemów"
+  //   comp: "bonus_system",
+  //   path: "/users_channels",
+  //   component: UsersChannels,
+  //   id: 27
+  //   // id: 20
+  //   // title: "Dodaj koszty"
+  // },
+  // {
+  //   comp: "users",
+  //   path: "/users",
+  //   component: Users,
+  //   id: 1
+  //   // title: "Użytkownicy"
+  //   //channel: auth ? auth.channel_first : 0
+  // },
+  // // {
+  // //   comp: "channels_config",
+  // //   path: "/configs",
+  // //   component: ChannelsConfig,
+  // //   title: "Konfiguracja systemów"
+  // //   //channel: auth ? auth.channel_first : 0
+  // // }
+  // {
+  //   comp: "invoices",
+  //   path: "/invoices",
+  //   component: Invoices,
+  //   id: 10
+  //   // title: "Zaległe faktury"
+  //   //channel: auth ? auth.channel_first : 0
+  // },
+  // {
+  //   comp: "customer_details",
+  //   path: "/customerdetails",
+  //   component: CustomerDetails,
+  //   // title: "Informacje o klientach",
+  //   // open: true,
+  //   id: 12
+  //   //channel: auth ? auth.channel_first : 0
+  // },
+  // {
+  //   comp: "calculators",
+  //   path: "/calculators",
+  //   component: Calculators,
+  //   id: 11
+  //   // title: "Kalkulatory"
+  //   // open: true
+  //   //channel: auth ? auth.channel_first : 0
+  // },
+  // {
+  //   comp: "settings",
+  //   path: "/settings",
+  //   component: Settings,
+  //   id: 21
+  //   // title: "Kalkulatory"
+  //   // open: true
+  //   //channel: auth ? auth.channel_first : 0
+  // },
+  // {
+  //   comp: "bonusRules",
+  //   path: "/bonusrules",
+  //   component: BonusRules,
+  //   id: 23
+  //   // title: "Kalkulatory"
+  //   // open: true
   //   //channel: auth ? auth.channel_first : 0
   // }
-  {
-    comp: "invoices",
-    path: "/invoices",
-    component: Invoices,
-    id: 10
-    // title: "Zaległe faktury"
-    //channel: auth ? auth.channel_first : 0
-  },
-  {
-    comp: "customer_details",
-    path: "/customerdetails",
-    component: CustomerDetails,
-    // title: "Informacje o klientach",
-    // open: true,
-    id: 12
-    //channel: auth ? auth.channel_first : 0
-  },
-  {
-    comp: "calculators",
-    path: "/calculators",
-    component: Calculators,
-    id: 11
-    // title: "Kalkulatory"
-    // open: true
-    //channel: auth ? auth.channel_first : 0
-  },
-  {
-    comp: "settings",
-    path: "/settings",
-    component: Settings,
-    id: 21
-    // title: "Kalkulatory"
-    // open: true
-    //channel: auth ? auth.channel_first : 0
-  },
-  {
-    comp: "bonusRules",
-    path: "/bonusrules",
-    component: BonusRules,
-    id: 23
-    // title: "Kalkulatory"
-    // open: true
-    //channel: auth ? auth.channel_first : 0
-  }
 ];
 
 export default routes;
