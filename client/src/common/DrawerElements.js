cbc4f4d5a3a5539748b20074bdd23b38f60d8aa2
Jacaplaca <dziewanowski@gmail.com>
2019-05-13 22:51:22 +0200

    adding elements

--- client/src/common/DrawerElements.js
+++ client/src/common/DrawerElements.js
@@ -1,144 +1,145 @@
 export const elements = auth => {
-  // const elementsStandard = [
-  //   {
-  //     comp: "powerBi",
-  //     text: "Power BI",
-  //     link: "https://powerbi.microsoft.com/pl-pl/",
-  //     icon: "Pageview",
-  //     links: []
-  //   },
-  //   {
-  //     comp: "nextReports",
-  //     text: "Next Reports",
-  //     link: "/nextreports",
-  //     icon: "InsertChartOutlined",
-  //     links: ["/nextreports"]
-  //   },
-  //   // {
-  //   //   text: "Planer",
-  //   //   icon: "EventIcon",
-  //   //   comp: "planer_reports",
-  //   //   links: ["/planer", "/raporty"],
-  //   //   menus: [
-  //   //     // {
-  //   //     //   comp: "nextReports",
-  //   //     //   text: "Next Reports",
-  //   //     //   link: "/nextreports",
-  //   //     //   icon: "InsertChartOutlined"
-  //   //     // }
-  //   //     {
-  //   //       // comp: "planer",
-  //   //       text: "Aktywności",
-  //   //       link: "/planer",
-  //   //       icon: "EventIcon"
-  //   //     },
-  //   //     {
-  //   //       // comp: "raporty",
-  //   //       text: "Raporty",
-  //   //       link: "/raporty",
-  //   //       icon: "EventAvailableIcon"
-  //   //     }
-  //   //   ]
-  //   // },
-  //   {
-  //     comp: "costs",
-  //     text: "Koszty",
-  //     link: "/costs",
-  //     icon: "MoneyIcon",
-  //     links: ["/costs"]
-  //   },
-  //   {
-  //     comp: "invoices",
-  //     text: "Faktury",
-  //     link: "/invoices",
-  //     icon: "ListAlt",
-  //     links: ["/invoices"]
-  //   },
-  //   {
-  //     comp: "customer_details",
-  //     text: "Informacje o klientach",
-  //     link: "/customerdetails",
-  //     icon: "People",
-  //     links: ["/customerdetails"]
-  //   },
-  //   {
-  //     comp: "calculators",
-  //     text: "Kalkulator premiowy",
-  //     link: "/calculators",
-  //     icon: "Money",
-  //     links: ["/calculators"]
-  //   },
-  //   {
-  //     comp: "bonusRules",
-  //     text: "Pliki do pobrania",
-  //     link: "/bonusrules",
-  //     icon: "Description",
-  //     links: ["/bonusrules"]
-  //   }
-  // ];
-  //
-  // const elementsKonfig = [
-  //   {
-  //     text: "Konfiguracja",
-  //     comp: "settings",
-  //     icon: "Settings",
-  //     links: [
-  //       "/products",
-  //       "/systems",
-  //       "/users_channels",
-  //       "/users",
-  //       "/settings"
-  //     ],
-  //     // link: "/costs",
-  //     comps: [
-  //       {
-  //         comp: "bonus_system",
-  //         // icon: "Assignment",
-  //         text: "Ustawienia premii",
-  //         menus: [
-  //           {
-  //             // comp: "products",
-  //             text: "Produkty",
-  //             link: "/products",
-  //             icon: "LocalOffer"
-  //           },
-  //           {
-  //             // comp: "channels",
-  //             text: "Systemy prowizyjne",
-  //             link: "/systems",
-  //             icon: "Assignment"
-  //           },
-  //           {
-  //             // comp: "users_channels",
-  //             text: "Pracownicy",
-  //             link: "/users_channels",
-  //             icon: "People"
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         comp: "users",
-  //         text: "Użytkownicy",
-  //         link: "/users",
-  //         icon: "Person",
-  //         links: ["/users"]
-  //       },
-  //       {
-  //         comp: "settings",
-  //         text: "Ogólne ustawienia",
-  //         link: "/settings",
-  //         icon: "Settings",
-  //         links: ["/settings"]
-  //       }
-  //     ]
-  //   }
-  // ];
+  const elementsStandard = [
+    {
+      comp: "powerBi",
+      text: "Power BI",
+      link: "https://powerbi.microsoft.com/pl-pl/",
+      icon: "Pageview",
+      links: []
+    },
+    {
+      comp: "nextReports",
+      text: "Next Reports",
+      link: "/nextreports",
+      icon: "InsertChartOutlined",
+      links: ["/nextreports"]
+    },
+    {
+      text: "Planer",
+      icon: "EventIcon",
+      comp: "planer_reports",
+      links: ["/planer", "/raporty"],
+      menus: [
+        // {
+        //   comp: "nextReports",
+        //   text: "Next Reports",
+        //   link: "/nextreports",
+        //   icon: "InsertChartOutlined"
+        // }
+        {
+          // comp: "planer",
+          text: "Aktywności",
+          link: "/planer",
+          icon: "EventIcon"
+        },
+        {
+          // comp: "raporty",
+          text: "Raporty",
+          link: "/raporty",
+          icon: "EventAvailableIcon"
+        }
+      ]
+    },
+    {
+      comp: "costs",
+      text: "Koszty",
+      link: "/costs",
+      icon: "MoneyIcon",
+      links: ["/costs"]
+    },
+    {
+      comp: "invoices",
+      text: "Faktury",
+      link: "/invoices",
+      icon: "ListAlt",
+      links: ["/invoices"]
+    },
+    {
+      comp: "customer_details",
+      text: "Informacje o klientach",
+      link: "/customerdetails",
+      icon: "People",
+      links: ["/customerdetails"]
+    },
+    {
+      comp: "calculators",
+      text: "Kalkulator premiowy",
+      link: "/calculators",
+      icon: "Money",
+      links: ["/calculators"]
+    },
+    {
+      comp: "bonusRules",
+      text: "Pliki do pobrania",
+      link: "/bonusrules",
+      icon: "Description",
+      links: ["/bonusrules"]
+    }
+  ];
 
-  // let elements = elementsStandard;
-  // elements =
-  //   auth && auth.clientId === 2
-  //     ? elementsStandard
-  //     : Object.assign(elementsStandard, elementsKonfig);
+  const elementsKonfig = [
+    {
+      text: "Konfiguracja",
+      comp: "settings",
+      icon: "Settings",
+      links: [
+        "/products",
+        "/systems",
+        "/users_channels",
+        "/users",
+        "/settings"
+      ],
+      // link: "/costs",
+      comps: [
+        {
+          comp: "bonus_system",
+          // icon: "Assignment",
+          text: "Ustawienia premii",
+          menus: [
+            {
+              // comp: "products",
+              text: "Produkty",
+              link: "/products",
+              icon: "LocalOffer"
+            },
+            {
+              // comp: "channels",
+              text: "Systemy prowizyjne",
+              link: "/systems",
+              icon: "Assignment"
+            },
+            {
+              // comp: "users_channels",
+              text: "Pracownicy",
+              link: "/users_channels",
+              icon: "People"
+            }
+          ]
+        },
+        {
+          comp: "users",
+          text: "Użytkownicy",
+          link: "/users",
+          icon: "Person",
+          links: ["/users"]
+        },
+        {
+          comp: "settings",
+          text: "Ogólne ustawienia",
+          link: "/settings",
+          icon: "Settings",
+          links: ["/settings"]
+        }
+      ]
+    }
+  ];
 
-  return [];
+  let elements = elementsStandard;
+  elements =
+    auth && auth.clientId === 2
+      ? elementsStandard
+      : // : Object.assign(elementsStandard, elementsKonfig);
+        [...elementsStandard, ...elementsKonfig];
+  return elements;
+  // return [];
 };
